922cf35c00494d278543e7f533ee1e4ac524ce56
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-11-10 15:28:11 -0700

    Adds log in page styles

--- src/scripts.js
+++ src/scripts.js
@@ -16,6 +16,9 @@ const userWelcome = document.getElementById('userWelcome');
 const userDashBoardLabel = document.getElementById('dashboardLabel');
 const calenderForBookingCont = document.getElementById('calanderForBookingContainer');
 const pastFutureBookings = document.getElementById('pastFutureBookings');
+const futureBookings = document.getElementById('futureBookings');
+const previousBookings = document.getElementById('pastBookings');
+const previousBookingsTable = document.getElementById('table-past-bookings');
 
 let customerData;
 let bookingData;
@@ -31,7 +34,7 @@ function getUpdatedPromiseData() {
     customerData = data[0].customers;
     bookingData = data[1].bookings;
     roomData = data[2].rooms;
-    customer = new Customer(customerData[16]);
+    customer = new Customer(customerData[8]);
     customer.totalMoneySpent = 0;
     customer.pastBookings = [];
     customer.upcomingBookings = [];
@@ -42,6 +45,7 @@ function getUpdatedPromiseData() {
     totalMoneySpent = customer.returnTotalMoneySpent(roomData);
     displayUserName();
     displayUserFutureBookings();
+    displayUserPastBookings();
   })
 }
 
@@ -64,5 +68,13 @@ function displayUserName() {
 
 function displayUserFutureBookings() {
  let future = mapBookings(upcomingBookings)
-  pastFutureBookings.innerHTML = `${future}`
+  futureBookings.innerHTML += `${future}`
 };
+
+function displayUserPastBookings() {
+  // previousBookings.innerHTML = `<p class='past-bookings-info'> ${ mapBookings(customer.pastBookings) }</p >`
+  // past.forEach(booking => {
+  //   previousBookingsTable.innerHTML += `<tr><td>${booking}</td></tr>`
+// });
+};
+
