ecae5c71af66bbdd88c6cce965ccf28dac1e631c
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-11-10 12:42:40 -0700

    Adds ids to connect to query selectors

--- src/scripts.js
+++ src/scripts.js
@@ -1,3 +1,4 @@
+//~~~Imports~~~
 import './css/styles.css';
 import './images/turing-logo.png'
 import './images/overlookLogo.png'
@@ -5,4 +6,8 @@ import './images/theOverlook.png'
 import './images/helpfulFrontDesk.png'
 import './images/eye-logo.png'
 
-
+//~~~QuerySelectors~~~
+const userWelcome = document.getElementById('userWelcome');
+const userDashBoardLabel = document.getElementById('userDashboardLabel');
+const calenderForBookingCont = document.getElementById('calanderForBookingContainer');
+const pastFutureBookings = document.getElementById('pastFutureBookings');
