4fbc14b4a613c0d5e54ae7c587a686baa69cfdf9
Thomas Peterson <thomasedwardpeterson@gmail.com>
2022-11-13 12:39:28 -0700

    Adds more dom adjustments

--- src/scripts.js
+++ src/scripts.js
@@ -29,8 +29,9 @@ const dashboardReturn = document.getElementById('backToDashboard');
 const showAllPastButton = document.getElementById('showAllPast');
 const showAllFutureButton = document.getElementById('showAllFuture');
 const allFutureBookingsPage = document.getElementById('allFutureBookingsPage'); 
-
-
+const mostRecentBookings = document.getElementById('mostRecentBookings');
+const mostRecentPastBookings = document.getElementById('mostRecentPastBookings');
+const returnToDash = document.getElementById('returnToDash');
 let customerData;
 let bookingData;
 let roomData;
@@ -40,19 +41,7 @@ let totalMoneySpent;
 let upcomingBookings;
 let hotel;
 
-// const getCustomerAndHotelData = (id) => {
-//   Promise.all([fetchData(`customers/${id}`), fetchData('bookings')])
-//     .then(data => {
-//       console.log(data)
-//       customer = new Customer({ customer: data[0], allBookings: data[1].bookings });
-//       console.log('check',customer.name)
-//       hotel = new Hotel({ allRooms: hotel.allRooms, allBookings: data[1].bookings });
-    
-      
-//     })
-//     .catch(error => {
 
-//     });
 const getPromiseData = () => {
   Promise.all([fetchData('customers'), fetchData('bookings'), fetchData('rooms')]).then(data => {
     customer = data[0].customers[0];
@@ -61,36 +50,101 @@ const getPromiseData = () => {
     customer = new Customer({ customer: customer, allBookings: bookingData });
     hotel = new Hotel({ allRooms: roomData, allBookings: bookingData });
     displayUserName();
-    // createFutureBookingsBox();
+    displayRecentBookings();
+    displayRecentBookingsPast();
   })
 };
-const createFutureBookingsBox = () => {
+
+
+const displayRecentBookings = () => {
+  customer.futureBookings.forEach((booking) => {
+   let roomToType = convertRoomNumberToType(booking.roomNumber)
+    mostRecentBookings.innerHTML += `
+    <article class='booking-card' id='bookingCard'>
+    <p class='dashboard-recent-bookings'> <span class='format'>Date</span>: ${booking.date}</p>
+    <p class='dashboard-recent-bookings'> <span class='format'>Room Number</span>: ${booking.roomNumber}</p>
+    <p class='dashboard-recent-bookings'> <span class='format'>Room Type</span>: ${roomToType.roomType}</p>
+    </article>`
+  })  
+};
+
+const displayRecentBookingsPast = () => {
+  console.log(customer.pastBookings)
+  customer.pastBookings.reverse().forEach((booking) => {
+    let roomToType = convertRoomNumberToType(booking.roomNumber)
+    mostRecentPastBookings.innerHTML = `
+    <article class='booking-card' id='bookingCard'> 
+    <p class='dashboard-recent-bookings'> <span class='format'>Date</span>: ${booking.date}</p>
+    <p class='dashboard-recent-bookings'> <span class='format'>Room Number</span>: ${booking.roomNumber}</p>
+    <p class='dashboard-recent-bookings'> <span class='format'>Room Type</span>: ${roomToType.roomType}</p>
+    </article>`
+  })
+};
+
+const createPastBookingsPage = () => {
+  hide([dashBoard, navBar]);
+  show([previousBookingsPage])
+  customer.pastBookings.forEach(booking => {
+    let roomToType = convertRoomNumberToType(booking.roomNumber)
+    previousBookingsPage.innerHTML += `
+        <article class="customer-booking-article past-bookings" id="${booking.id}">
+            <p class="booking-info">ID: ${booking.id}</p>
+            <p class="booking-info">Date: ${makeDateDisplay(booking.date)}</p>
+            <p class="booking-info">Room Number: ${booking.roomNumber}</p>
+            <p class="booking-info">Room Type: ${roomToType.roomType}</p>
+            <p class="booking-info">This Room Has ${roomToType.numBeds} ${roomToType.bedSize} beds.</p>
+            <button class="cancel-btn" id="${booking.id}" data-bookingID="${booking.id}">Cancel Booking</button>
+        </article>
+            `;
+})}
+
+const dashReturnRefresh = () => {
+  hide([previousBookingsPage]);
+  show([dashBoard, navBar]);
+  console.log('did it work?')
+};
+
+const convertRoomNumberToType = (roomNumber) => {
+let roomDeets = hotel.allRooms.find((room) => {
+   return room.number === roomNumber
+  })
+  return roomDeets
+}
+
+const createFutureBookingsPage = () => {
   hide([dashBoard, navBar]);
   show([allFutureBookingsPage])
   customer.futureBookings.forEach(booking => {
+   let roomToType = convertRoomNumberToType(booking.roomNumber)
       allFutureBookingsPage.innerHTML += (`
-                <article class="customer-booking-article future-bookings" id="${booking.id}">
-                    <p class="booking-info">ID: ${booking.id}</p>
-                    <p class="booking-info">Date: ${makeDateDisplay(booking.date)}</p>
-                    <p class="booking-info">Room Number: ${booking.roomNumber}</p>
-                    <button class="cancel-btn" id="${booking.id}" data-bookingID="${booking.id}">Cancel</button>
-                </article>
+        <article class="customer-booking-article future-bookings" id="${booking.id}">
+            <p class="booking-info">ID: ${booking.id}</p>
+            <p class="booking-info">Date: ${makeDateDisplay(booking.date)}</p>
+            <p class="booking-info">Room Number: ${booking.roomNumber}</p>
+            <p class="booking-info">Room Type: ${roomToType.roomType}</p>
+            <p class="booking-info">This Room Has ${roomToType.numBeds} ${roomToType.bedSize} beds.</p>
+            <button class="cancel-btn" id="${booking.id}" data-bookingID="${booking.id}">Cancel Booking</button>
+        </article>
             `);
   });
 }
-  window.addEventListener('load', getPromiseData);
-showAllFutureButton.addEventListener('click',createFutureBookingsBox);
-//~~Event Listeners~~
 
+
+//~~Event Listeners~~
+window.addEventListener('load', getPromiseData);
+showAllFutureButton.addEventListener('click',createFutureBookingsPage);
+showAllPastButton.addEventListener('click', createPastBookingsPage);
+returnToDash.addEventListener('click', dashReturnRefresh);
 calenderSubmitButton.addEventListener('click', (event) => {
   let selectedDate = actualCalender.value
   makeDateDisplay(selectedDate);
   console.log('does anything work?')
-})
+});
+
+
 //~~Helper Functions~~
 const hide = (elements) => elements.forEach(element => element.classList.add('hidden'));
 const show = (elements) => elements.forEach(element => element.classList.remove('hidden'));
-
 const bookedMessage = `<h1>The Overlook at Mordor awaits your arival, the room is booked!</h1>`
 const canceledMessage = `<h1>The Overlook at Mordor hopes to see you soon, the room is cancled!</h1>`
 
@@ -110,14 +164,14 @@ const makeDateDisplay = (date) => {
   return dateNumbers.join('/');
 };
 
-function mapBookings(bookingsArray) {
-  let mappedBookings = ''
-  mappedBookings = bookingsArray.map((booking) => {
-    return `<p class='past-booking-info'> Date: ${booking.date}  <br />
-    Room Number: ${booking.roomNumber} <br /> </p>`
-  }).join('')
-  return mappedBookings
-}
+// function mapBookings(bookingsArray) {
+//   let mappedBookings = ''
+//   mappedBookings = bookingsArray.map((booking) => {
+//     return `<p class='past-booking-info'> Date: ${booking.date}  <br />
+//     Room Number: ${booking.roomNumber} <br /> </p>`
+//   }).join('')
+//   return mappedBookings
+// }
 
 function displayUserName() {
   userWelcome.innerText = `Welcome, ${customer.name} you have spent $${customer.returnTotalSpent(hotel.allRooms)} this pleases our Dark Lord! `
@@ -131,18 +185,9 @@ function displayUserName() {
 
 
 
-// const createPastBookingsBox = () => {
-//   customer.pastBookings.forEach(booking => {
-//     previousBookings.innerHTML += (`
-//     <article class="customer-booking-article">
-//     <p class="information">ID: ${booking.id}</p>
-//     <p class="information">Date: ${makeDateDisplay(booking.date)}</p>
-//     <p class="information">Room Number: ${booking.roomNumber}</p>
-//     </article>
-//     `);
-//   });
 
-// }
+
+
 
  
 
